72c6247733b3e65cf0a2d4d3fe6c8940d327d6f7
elzahraa saeed <elzahraa.morsy.99@gmail.com>
2023-03-16 19:23:09 -0700

    q4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description :  A c programming that prints word eith puts function
+ *
+ * Return : Always 0 (sucess)
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
