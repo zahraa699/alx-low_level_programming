d9ea01d992289abd1434a9e55b9db804dc1d007f
elzahraa saeed <elzahraa.morsy.99@gmail.com>
2023-03-17 18:57:09 -0700

    q2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print alphbet letter
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
