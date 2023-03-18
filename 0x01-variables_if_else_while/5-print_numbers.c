10d848ac814bdea282d2a05cef64357300dff7e2
elzahraa saeed <elzahraa.morsy.99@gmail.com>
2023-03-17 19:38:29 -0700

    q5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints number from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int num = 0;
+for (num = 0; num < 10; num++)
+{
+printf("%i", num);
+}
+printf("\n");
+return (0);
+}
