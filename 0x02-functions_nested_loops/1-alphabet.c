89ec38e0ed048ff832a532be1a104fd3479172f9
elzahraa saeed <elzahraa.morsy.99@gmail.com>
2023-03-21 16:18:35 -0700

    q1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Description : print_alphabet from _putchar fuction
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+return (0);
+}
