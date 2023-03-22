4f7e3443f59dd86035112f0e7430eca90b17c8b0
elzahraa saeed <elzahraa.morsy.99@gmail.com>
2023-03-21 19:57:49 -0700

    q15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description : prints first 50 firbrac numbers
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	float total_sum;
+
+	while (1)
+	{
+		sum = fib1 + fib2;
+		if (sum > 4000000)
+			break;
+		if ((sum % 2) == 0)
+			total_sum += sum;
+		fib1 = fib2;
+		fib2 = sum;
+	}
+	printf("%.0f\n", total_sum);
+	return (0);
+}
