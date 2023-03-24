596ab3389c5bd2fca5e4f55d92faf9f6cf403d41
elzahraa saeed <elzahraa.morsy.99@gmail.com>
2023-03-23 19:22:42 -0700

    q0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,29 +1,9 @@
 #include "main.h"
 
 /**
- * positive_or_negative - tests function that prints if int is + or -
- * @n: Description of parameter n
- *
- * Return: Always 0 (Success)
- */
-
-void positive_or_negative(int n)
-{
-	if (n > 0)
-		printf("%i is positive\n", n);
-	else if (n == 0)
-		printf("%i is zero\n", n);
-	else
-		printf("%i is negative\n", n);
-}
-
-/**
- * main - Entry point
- *
- * Description:tests function that prints if integer is positive or negative
- *
- * Return: Always 0 (Success)
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
@@ -31,5 +11,6 @@ int i;
 
 i = 98 % 2;
 positive_or_negative(i);
+
 return (0);
 }
