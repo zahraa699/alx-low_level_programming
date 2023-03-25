ef2322333ce8c31c46495edc9b4d627522fd3f8c
elzahraa saeed <elzahraa.morsy.99@gmail.com>
2023-03-24 18:21:48 -0700

    q7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,16 +8,24 @@
 
 void print_diagonal(int n)
 {
-	int i, space;
+	int i, j;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (i = 1; i <= n; i++)
-			_putchar(' ');
-		_putchar(92); /* asci equal '/' */
-		_putchar('\n');
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+			
 	}
 
 }
