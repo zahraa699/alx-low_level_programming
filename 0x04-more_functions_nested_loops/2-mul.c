cf818a582679cfe4242dd26af36414ea311bf098
elzahraa saeed <elzahraa.morsy.99@gmail.com>
2023-03-24 17:08:57 -0700

    q2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Short description, single line
+ * @a : first input
+ * @b : second input
+ *
+ * Return: Description of the returned value
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
