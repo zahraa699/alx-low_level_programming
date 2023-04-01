296e1b7a62eceb03eee0d26f03e4a89138215684
elzahraa saeed <elzahraa.morsy.99@gmail.com>
2023-03-31 17:05:37 -0700

    q14 adv

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -3,43 +3,48 @@
 
 /**
  * print_buffer - prints buffer
- * @b :buffer
- * @size :size
- * return : void
-*/
+ * @b: buffer
+ * @size: size
+ * Return: void
+ */
 
 void print_buffer(char *b, int size)
 {
-	int i, j, k;
+int o, j, i;
 
-	k = 0;
-	if (size <= 0)
-	{
-		printf("\n");
-		return;
-	}
-	while (k < size)
-	{
-		j = size - k < 10 ? size - k : 10;
-		printf("%08x: ", k);
-		for (i = 0; i < 10; i++)
-		{
-			if (i < j)
-				printf("%02x", *(b + k + i));
-			else
-				printf(" ");
-			if (i % 2)
-				printf(" ");
-		}
-		for (i = 0; i < j; i++)
-		{
-			int c = *(b + k + i);
+o = 0;
 
-			if (c < 32 || c > 132)
-				c = '.';
-			printf("%c", c);
-		}
-		printf("\n");
-		k += 10;
-	}
+if (size <= 0)
+{
+printf("\n");
+return;
+}
+while (o < size)
+{
+j = size - o < 10 ? size - o : 10;
+printf("%08x: ", o);
+for (i = 0; i < 10; i++)
+{
+if (i < j)
+printf("%02x", *(b + o + i));
+else
+printf("  ");
+if (i % 2)
+{
+printf(" ");
+}
+}
+for (i = 0; i < j; i++)
+{
+int c = *(b + o + i);
+
+if (c < 32 || c > 132)
+{
+c = '.';
+}
+printf("%c", c);
+}
+printf("\n");
+o += 10;
+}
 }
