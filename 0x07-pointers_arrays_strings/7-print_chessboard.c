7a498c1e478f00b84c69c7854cd9da78dda77da2
elzahraa saeed <elzahraa.morsy.99@gmail.com>
2023-04-03 19:02:11 -0700

    q6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - this function print chessboard
+ * @a : matrix
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int j, z;
+
+	for (j = 0; j < 8; j++)
+	{
+		for (z = 0; z < 8; z++)
+		{
+			_putchar(a[j][z]);
+		}
+		_putchar('\n');
+	}
+}
