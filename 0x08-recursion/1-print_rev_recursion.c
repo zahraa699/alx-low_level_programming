9607322104a0cb8272159a14226beeb0a7c6d7fa
elzahraa saeed <elzahraa.morsy.99@gmail.com>
2023-04-05 15:08:18 -0700

    q1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints string inreverse
+ * @s: pointer to string
+ *
+ * Description: Longer description of the function)
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
