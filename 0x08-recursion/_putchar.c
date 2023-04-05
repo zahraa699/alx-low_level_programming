45de641a9efe011e712bb81a643fbe9fa7bca55d
elzahraa saeed <elzahraa.morsy.99@gmail.com>
2023-04-05 14:17:49 -0700

    q0

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * Description : _putchar -writes the character c to stdout
+ * @c: the character to print
+ *
+ * Description : _putchar -writes the character c to stdout
+ *
+ * Return: on sucess 1 , on error -1 is returned
+ */
+
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
