d0ce3ad39d8fe67504907610ea6afb7619549061
elzahraa saeed <elzahraa.morsy.99@gmail.com>
2023-04-18 18:36:58 -0700

    q0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef FILE_DOG_H
+#define FILE_DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name : dog name
+ * @age : age of dog
+ * @owner : owner's name of dog
+ *
+ * Description: just along dog struct in kitty world
+ *
+ * Return: Description of the returned value
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
