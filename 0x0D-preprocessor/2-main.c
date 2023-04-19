60e071c3ef52e5b13099495c2ea6e5cda2c83225
elzahraa saeed <elzahraa.morsy.99@gmail.com>
2023-04-18 18:24:06 -0700

    fix q2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - print name of this source file
- *
- * Return: the returned value is zero
+ *main - check the code.
+ *Return:Always 0.
  */
 
 int main(void)
 {
-	printf("%s\n", _FILE_);
-	return (0);
+printf("%s\n", __FILE__);
+return (0);
 }
