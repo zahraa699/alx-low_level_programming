982595f533bd8a6b9f18d9818b458c9bf3a4be35
elzahraa saeed <elzahraa.morsy.99@gmail.com>
2023-04-20 19:43:55 -0700

    q3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,9 +3,27 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+
+/**
+ * struct token - struct token
+ * @token: format token
+ * @f : the function asscoi
+ *
+ * Description: Longer description of the function
+ *
+ * Return: Description of the returned value
+ */
+
+
+typedef struct token
+{
+	char *token;
+	void (*f)(char *, va_list);
+} token_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-
-
+void print_all(const char * const format, ...);
+int _putchar(char c);
 #endif
