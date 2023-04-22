64ff6fe1110d0a795c46d36306231b858d6b4757
elzahraa saeed <elzahraa.morsy.99@gmail.com>
2023-04-21 19:14:11 -0700

    q3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,35 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * get_op_func - this function get an operator
+ * @s: the operator passed as arg to the program
+ *
+ * Description: Longer description of the function)?
+ *
+ * Return: Description of the returned value
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i;
+
+while (i < 5)
+{
+if (s && s[0] == ops[i].op[0] && !s[1])
+return (ops[i].f);
+i++
+}
+return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - this function add two numbers
+ * @a : the first number
+ * @b : second number
+ *
+ * Description: Longer description of the function)?
+ *
+ * Return: the returned value is sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - this function sub two numbers
+ * @a : the first number
+ * @b : second number
+ *
+ * Description: Longer description of the function)?
+ *
+ * Return: the returned value is sub
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_div - this function divied two numbers
+ * @a : the first number
+ * @b : second number
+ *
+ * Description: Longer description of the function)?
+ *
+ * Return: the returned value is divied result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - this function add two numbers
+ * @a : the first number
+ * @b : second number
+ *
+ * Description: Longer description of the function)?
+ *
+ * Return: the returned value of remainder
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
