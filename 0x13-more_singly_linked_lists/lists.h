54d326a6cf6a37205a0810b2774f0d052ede3314
elzahraa saeed <elzahraa.morsy.99@gmail.com>
2023-05-02 15:00:34 -0700

    adv.q13

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -36,5 +36,7 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 const listint_t **_r(const listint_t **list, size_t size, const listint_t *new);
+listint_t *find_listint_loop_fl(listint_t *head);
+
 
 #endif
